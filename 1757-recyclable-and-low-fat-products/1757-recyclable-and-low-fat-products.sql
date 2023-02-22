2100bc0db297d91fd94280e257a0865027bb0582
Zaid Nasim Khan <zkhan0245@gmail.com>
2023-02-22 18:49:52 +0530

    Time: 1017 ms (42.39%), Space: 0B (100.00%) - LeetHub

--- 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
+++ 1757-recyclable-and-low-fat-products/1757-recyclable-and-low-fat-products.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select product_id from Products
-where low_fats='Y'
-and recyclable='Y';
+select p.product_id from Products p
+where p.low_fats='Y'
+and p.recyclable='Y';
